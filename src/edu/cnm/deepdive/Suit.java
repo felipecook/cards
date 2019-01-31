592386ebfaa9bf88dcf6d0cb46fa95d344451c6f
Felipe Cook <felipepcook@gmail.com>
2019-01-31 13:37:03 -0700

    Initial commit

---
+++ src/edu/cnm/deepdive/Suit.java
@@ -0,0 +1,29 @@
+package edu.cnm.deepdive;
+
+/**This enum implements suits of standard playing cards.
+ * Each enumerated value, uses the standard symbol for that suit (taken from Unicode),
+ * as its string representation.
+ *
+ * @author Felipe Cook &amp; Nicholas Bennett
+ * @version 1.0
+ */
+
+public enum Suit {
+
+  CLUBS("\u2663"),
+  DIAMONDS("\u2662"),
+  HEARTS("\u2661"),
+  SPADES("\u2660");
+
+  private final String symbol;
+
+  Suit(String symbol) {
+    this.symbol = symbol;
+  }
+
+
+  @Override
+  public String toString() {
+    return symbol;
+  }
+}
